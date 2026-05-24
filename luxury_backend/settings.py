d6f0c65844bf27d1eb6d36ffef708d413f0fef59
Edisson <edissonrivasherrera@gmail.com>
2026-05-23 21:01:30 -0500

    fix jwt authentication

--- luxury_backend/settings.py
+++ luxury_backend/settings.py
@@ -82,10 +82,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # CORS — permite que Flutter se conecte
 CORS_ALLOW_ALL_ORIGINS = True
 
-# REST Framework
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticated',
